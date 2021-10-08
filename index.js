afd252f9b22f7a8bd02dcf44dbae517347bfa7a8
Guilherme Alves Ceobaniuk Zaluchi <guiga_zalu@hotmail.com.br>
2021-10-08 18:58:45 -0300

    Discovered what is Prettier.
    Discovered what is Prettier, and used it. Might reconfigure, with a .prettierrc file, I guess.

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const	core = require("@actions/core"),
-		fs = require("fs"),
-		ini = require("ini"),
-		{ spawn } = require("child_process"),
-		{ Toolkit } = require("actions-toolkit");
+const core = require("@actions/core"),
+  fs = require("fs"),
+  ini = require("ini"),
+  { spawn } = require("child_process"),
+  { Toolkit } = require("actions-toolkit");
 
 // Get config
-const GH_USERNAME	= core.getInput("GH_USERNAME");
-const COMMIT_MSG	= core.getInput("COMMIT_MSG");
-const MAX_LINES	= core.getInput("MAX_LINES");
-const LANG		= core.getInput("LANG");
+const GH_USERNAME = core.getInput("GH_USERNAME");
+const COMMIT_MSG = core.getInput("COMMIT_MSG");
+const MAX_LINES = core.getInput("MAX_LINES");
+const LANG = core.getInput("LANG");
 
-const texts = (function(){
-	var file;
-	// More languages in the future (maybe)
-	switch(LANG.toLowerCase()){
-		case "pt-BR":
-		case "pt-EU":
-		case "pt":
-			file = "./lang/pt-BR.ini";
-			break;
-		case "en-US":
-		case "en-GB":
-		case "en":
-		default:
-			file = "./lang/en-US.ini";
-			break;
-	}
-	return ini.decode(fs.readFileSync(file));
+const texts = (function () {
+  var file;
+  // More languages in the future (maybe)
+  switch (LANG.toLowerCase()) {
+    case "pt-BR":
+    case "pt-EU":
+    case "pt":
+      file = "./lang/pt-BR.ini";
+      break;
+    case "en-US":
+    case "en-GB":
+    case "en":
+    default:
+      file = "./lang/en-US.ini";
+      break;
+  }
+  return ini.decode(fs.readFileSync(file));
 })();
 /**
  * Returns the sentence case representation
@@ -47,12 +47,12 @@ const urlPrefix = "https://github.com";
  */
 
 const toUrlFormat = (item) => {
-	if(typeof item === "object")
-		return Object.hasOwnProperty.call(item.payload, "issue")
-			? `[#${item.payload.issue.number}](${urlPrefix}/${item.repo.name}/issues/${item.payload.issue.number})`
-			: `[#${item.payload.pull_request.number}](${urlPrefix}/${item.repo.name}/pull/${item.payload.pull_request.number})`;
+  if (typeof item === "object")
+    return Object.hasOwnProperty.call(item.payload, "issue")
+      ? `[#${item.payload.issue.number}](${urlPrefix}/${item.repo.name}/issues/${item.payload.issue.number})`
+      : `[#${item.payload.pull_request.number}](${urlPrefix}/${item.repo.name}/pull/${item.payload.pull_request.number})`;
 
-	return `[${item}](${urlPrefix}/${item})`;
+  return `[${item}](${urlPrefix}/${item})`;
 };
 
 /**
@@ -64,22 +64,22 @@ const toUrlFormat = (item) => {
  */
 
 const exec = (cmd, args = []) =>
-	new Promise((resolve, reject) => {
-		const app = spawn(cmd, args, { stdio: "pipe" });
-		let stdout = "";
-		app.stdout.on("data", (data) => {
-			stdout = data;
-		});
-		app.on("close", (code) => {
-			if(code === 0 || stdout.includes("nothing to commit"))
-				return resolve(code);
-			
-			err = new Error(`Invalid status code: ${code}`);
-			err.code = code;
-			return reject(err);
-		});
-		app.on("error", reject);
-	});
+  new Promise((resolve, reject) => {
+    const app = spawn(cmd, args, { stdio: "pipe" });
+    let stdout = "";
+    app.stdout.on("data", (data) => {
+      stdout = data;
+    });
+    app.on("close", (code) => {
+      if (code === 0 || stdout.includes("nothing to commit"))
+        return resolve(code);
+
+      err = new Error(`Invalid status code: ${code}`);
+      err.code = code;
+      return reject(err);
+    });
+    app.on("error", reject);
+  });
 
 /**
  * Make a commit
@@ -88,171 +88,171 @@ const exec = (cmd, args = []) =>
  */
 
 const commitFile = async () => {
-	await exec("git", [
-		"config",
-		"--global",
-		"user.email",
-		"41898282+github-actions[bot]@users.noreply.github.com",
-	]);
-	await exec("git", ["config", "--global", "user.name", "readme-bot"]);
-	await exec("git", ["add", "README.md"]);
-	await exec("git", ["commit", "-m", COMMIT_MSG]);
-	await exec("git", ["push"]);
+  await exec("git", [
+    "config",
+    "--global",
+    "user.email",
+    "41898282+github-actions[bot]@users.noreply.github.com",
+  ]);
+  await exec("git", ["config", "--global", "user.name", "readme-bot"]);
+  await exec("git", ["add", "README.md"]);
+  await exec("git", ["commit", "-m", COMMIT_MSG]);
+  await exec("git", ["push"]);
 };
 
 const serializers = {
-	IssueCommentEvent(item){
-		return texts.IssueCommentEvent
-			.replace("$item_repo_name", toUrlFormat(item.repo.name))
-			.replace("$item", toUrlFormat(item));
-	},
-	IssuesEvent(item){
-		return texts.IssuesEvent
-			.replace("$item_payload_action", capitalize(item.payload.action))
-			.replace("$item_repo_name", toUrlFormat(item.repo.name))
-			.replace("$item", toUrlFormat(item));
-	},
-	PullRequestEvent(item){
-		if(item.payload.pull_request.merged)
-			return texts.PullRequestEvent.merge
-				.replace("$item_repo_name", toUrlFormat(item.repo.name))
-				.replace("$item", toUrlFormat(item));
-		
-		if(item.payload.action === "opened")
-			return texts.PullRequestEvent.open
-				.replace("$item_payload_action", capitalize(item.payload.action))
-				.replace("$item_repo_name", toUrlFormat(item.repo.name))
-				.replace("$item", toUrlFormat(item));
-		
-		return texts.PullRequestEvent.close
-			.replace("$item_payload_action", capitalize(item.payload.action))
-			.replace("$item_repo_name", toUrlFormat(item.repo.name))
-			.replace("$item", toUrlFormat(item));
-	}
+  IssueCommentEvent(item) {
+    return texts.IssueCommentEvent.replace(
+      "$item_repo_name",
+      toUrlFormat(item.repo.name)
+    ).replace("$item", toUrlFormat(item));
+  },
+  IssuesEvent(item) {
+    return texts.IssuesEvent.replace(
+      "$item_payload_action",
+      capitalize(item.payload.action)
+    )
+      .replace("$item_repo_name", toUrlFormat(item.repo.name))
+      .replace("$item", toUrlFormat(item));
+  },
+  PullRequestEvent(item) {
+    if (item.payload.pull_request.merged)
+      return texts.PullRequestEvent.merge
+        .replace("$item_repo_name", toUrlFormat(item.repo.name))
+        .replace("$item", toUrlFormat(item));
+
+    if (item.payload.action === "opened")
+      return texts.PullRequestEvent.open
+        .replace("$item_payload_action", capitalize(item.payload.action))
+        .replace("$item_repo_name", toUrlFormat(item.repo.name))
+        .replace("$item", toUrlFormat(item));
+
+    return texts.PullRequestEvent.close
+      .replace("$item_payload_action", capitalize(item.payload.action))
+      .replace("$item_repo_name", toUrlFormat(item.repo.name))
+      .replace("$item", toUrlFormat(item));
+  },
 };
 
-async function toolkitFn(tools){
-	// Get the user's public events
-	tools.log.debug(`Getting activity for ${GH_USERNAME}`);
-	const events = await tools.github.activity.listPublicEventsForUser({
-		username: GH_USERNAME,
-		per_page: 100,
-	});
-	tools.log.debug(
-		`Activity for ${GH_USERNAME}, ${events.data.length} events found.`
-	);
-	
-	const content = events.data
-		// Filter out any boring activity
-		.filter((event) => serializers.hasOwnProperty(event.type))
-		// We only have 'MAX_LINES' lines to work with
-		.slice(0, MAX_LINES)
-		// Call the serializer to construct a string
-		.map((item) => serializers[item.type](item));
-	
-	const readmeContent = fs.readFileSync("./README.md", "utf-8").split("\n");
-	
-	// Find the index corresponding to <!--START_SECTION:activity--> comment
-	let startIdx = readmeContent.findIndex(
-		(content) => content.trim() === "<!--START_SECTION:activity-->"
-	);
-	
-	// Early return in case the <!--START_SECTION:activity--> comment was not found
-	if(startIdx === -1)
-		return tools.exit.failure(
-			`Couldn't find the <!--START_SECTION:activity--> comment. Exiting!`
-		);
-	
-	// Find the index corresponding to <!--END_SECTION:activity--> comment
-	const endIdx = readmeContent.findIndex(
-		(content) => content.trim() === "<!--END_SECTION:activity-->"
-	);
-	
-	if(!content.length)
-		tools.exit.failure("No PullRequest/Issue/IssueComment events found");
-	
-	if(content.length < MAX_LINES)
-		tools.log.info(`Found less than MAX_LINES=${MAX_LINES} activities`);
-	
-	if(startIdx !== -1 && endIdx === -1){
-		// Add one since the content needs to be inserted just after the initial comment
-		startIdx++;
-		content.forEach((line, idx) =>
-			readmeContent.splice(startIdx + idx, 0, `${idx + 1}. ${line}`)
-		);
-		
-		// Append <!--END_SECTION:activity--> comment
-		readmeContent.splice(
-			startIdx + content.length, 0, "<!--END_SECTION:activity-->"
-		);
-		
-		// Update README
-		fs.writeFileSync("./README.md", readmeContent.join("\n"));
-		
-		// Commit to the remote repository
-		try{
-			await commitFile();
-		}catch(err){
-			tools.log.debug("Something went wrong");
-			return tools.exit.failure(err);
-		}
-		tools.exit.success("Wrote to README");
-	}
-	
-	const oldContent = readmeContent.slice(startIdx + 1, endIdx).join("\n");
-	const newContent = content
-		.map((line, idx) => `${idx + 1}. ${line}`)
-		.join("\n");
-	
-	if(oldContent.trim() === newContent.trim())
-		tools.exit.success("No changes detected");
-	
-	startIdx++;
-	
-	// Recent GitHub Activity content between the comments
-	const readmeActivitySection = readmeContent.slice(startIdx, endIdx);
-	if(!readmeActivitySection.length){
-		content.some((line, idx) => {
-			// User doesn't have 'MAX_LINES' public events
-			if(!line)
-				return true;
-			
-			readmeContent.splice(startIdx + idx, 0, `${idx + 1}. ${line}`);
-		});
-		tools.log.success("Wrote to README");
-	}else{
-		// It is likely that a newline is inserted after the <!--START_SECTION:activity--> comment (code formatter)
-		let count = 0;
-		
-		readmeActivitySection.some((line, idx) => {
-			// User doesn't have 'MAX_LINES' public events
-			if(!content[count])
-				return true;
-			
-			if(line !== ""){
-				readmeContent[startIdx + idx] = `${count + 1}. ${content[count]}`;
-				count++;
-			}
-		});
-		tools.log.success("Updated README with the recent activity");
-	}
-	
-	// Update README
-	fs.writeFileSync("./README.md", readmeContent.join("\n"));
-	
-	// Commit to the remote repository
-	try{
-		await commitFile();
-	}catch(err){
-		tools.log.debug("Something went wrong");
-		return tools.exit.failure(err);
-	}
-	tools.exit.success("Pushed to remote repository");
+async function toolkitFn(tools) {
+  // Get the user's public events
+  tools.log.debug(`Getting activity for ${GH_USERNAME}`);
+  const events = await tools.github.activity.listPublicEventsForUser({
+    username: GH_USERNAME,
+    per_page: 100,
+  });
+  tools.log.debug(
+    `Activity for ${GH_USERNAME}, ${events.data.length} events found.`
+  );
+
+  const content = events.data
+    // Filter out any boring activity
+    .filter((event) => serializers.hasOwnProperty(event.type))
+    // We only have 'MAX_LINES' lines to work with
+    .slice(0, MAX_LINES)
+    // Call the serializer to construct a string
+    .map((item) => serializers[item.type](item));
+
+  const readmeContent = fs.readFileSync("./README.md", "utf-8").split("\n");
+
+  // Find the index corresponding to <!--START_SECTION:activity--> comment
+  let startIdx = readmeContent.findIndex(
+    (content) => content.trim() === "<!--START_SECTION:activity-->"
+  );
+
+  // Early return in case the <!--START_SECTION:activity--> comment was not found
+  if (startIdx === -1)
+    return tools.exit.failure(
+      `Couldn't find the <!--START_SECTION:activity--> comment. Exiting!`
+    );
+
+  // Find the index corresponding to <!--END_SECTION:activity--> comment
+  const endIdx = readmeContent.findIndex(
+    (content) => content.trim() === "<!--END_SECTION:activity-->"
+  );
+
+  if (!content.length)
+    tools.exit.failure("No PullRequest/Issue/IssueComment events found");
+
+  if (content.length < MAX_LINES)
+    tools.log.info(`Found less than MAX_LINES=${MAX_LINES} activities`);
+
+  if (startIdx !== -1 && endIdx === -1) {
+    // Add one since the content needs to be inserted just after the initial comment
+    startIdx++;
+    content.forEach((line, idx) =>
+      readmeContent.splice(startIdx + idx, 0, `${idx + 1}. ${line}`)
+    );
+
+    // Append <!--END_SECTION:activity--> comment
+    readmeContent.splice(
+      startIdx + content.length,
+      0,
+      "<!--END_SECTION:activity-->"
+    );
+
+    // Update README
+    fs.writeFileSync("./README.md", readmeContent.join("\n"));
+
+    // Commit to the remote repository
+    try {
+      await commitFile();
+    } catch (err) {
+      tools.log.debug("Something went wrong");
+      return tools.exit.failure(err);
+    }
+    tools.exit.success("Wrote to README");
+  }
+
+  const oldContent = readmeContent.slice(startIdx + 1, endIdx).join("\n");
+  const newContent = content
+    .map((line, idx) => `${idx + 1}. ${line}`)
+    .join("\n");
+
+  if (oldContent.trim() === newContent.trim())
+    tools.exit.success("No changes detected");
+
+  startIdx++;
+
+  // Recent GitHub Activity content between the comments
+  const readmeActivitySection = readmeContent.slice(startIdx, endIdx);
+  if (!readmeActivitySection.length) {
+    content.some((line, idx) => {
+      // User doesn't have 'MAX_LINES' public events
+      if (!line) return true;
+
+      readmeContent.splice(startIdx + idx, 0, `${idx + 1}. ${line}`);
+    });
+    tools.log.success("Wrote to README");
+  } else {
+    // It is likely that a newline is inserted after the <!--START_SECTION:activity--> comment (code formatter)
+    let count = 0;
+
+    readmeActivitySection.some((line, idx) => {
+      // User doesn't have 'MAX_LINES' public events
+      if (!content[count]) return true;
+
+      if (line !== "") {
+        readmeContent[startIdx + idx] = `${count + 1}. ${content[count]}`;
+        count++;
+      }
+    });
+    tools.log.success("Updated README with the recent activity");
+  }
+
+  // Update README
+  fs.writeFileSync("./README.md", readmeContent.join("\n"));
+
+  // Commit to the remote repository
+  try {
+    await commitFile();
+  } catch (err) {
+    tools.log.debug("Something went wrong");
+    return tools.exit.failure(err);
+  }
+  tools.exit.success("Pushed to remote repository");
 }
-Toolkit.run(
-	toolkitFn,
-	{
-		event: ["schedule", "workflow_dispatch"],
-		secrets: ["GITHUB_TOKEN"],
-	}
-);
+Toolkit.run(toolkitFn, {
+  event: ["schedule", "workflow_dispatch"],
+  secrets: ["GITHUB_TOKEN"],
+});
